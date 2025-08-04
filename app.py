fdb4696f2e7610d5c48f7265522fe7697eaa67bb
dvir-berko <berko.faibish.dvir@gmail.comgit config --global user.email berko.faibish.dvir@gmail.com>
2025-08-05 00:20:26 +0300

    Update project files

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
+import random
+
 orders = []
+example_orders = [
+    {"destination": "Japan", "start_date": "2025-10-01", "end_date": "2025-10-14"},
+    {"destination": "Italy", "start_date": "2025-11-01", "end_date": "2025-11-10"},
+    {"destination": "Spain", "start_date": "2025-09-15", "end_date": "2025-09-25"},
+    {"destination": "France", "start_date": "2025-12-01", "end_date": "2025-12-10"}
+]
+
+@app.route('/', methods=['GET'])
+def root():
+    return jsonify({"message": "Welcome to the API! Try /orders"}), 200
 
 @app.route('/orders', methods=['GET', 'POST'])
 def orders_endpoint():
@@ -10,6 +22,12 @@ def orders_endpoint():
         new_order['id'] = len(orders) + 1
         orders.append(new_order)
         return jsonify(new_order), 201
+        if not orders:
+        sample = random.sample(example_orders, k=2)
+        for item in sample:
+            item = item.copy()
+            item['id'] = len(orders) + 1
+            orders.append(item)
     return jsonify(orders), 200
 
 if __name__ == '__main__':
